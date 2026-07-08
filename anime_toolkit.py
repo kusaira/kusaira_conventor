86100ce83318ffc1857e4e3cb8cad3c7244613af
keqqy <keqqy@example.com>
2026-07-08 22:32:30 +0300

    Bump version to v2.1

--- anime_toolkit.py
+++ anime_toolkit.py
@@ -538,7 +538,7 @@ def main():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("=============================================")
-        print("          Kusaira Conventor v2.0             ")
+        print("          Kusaira Conventor v2.1             ")
         print("=============================================")
         print("1. Конвертация всех видео в MP4 (HEVC + AAC)")
         print("2. Safe Mode (Удалить/Извлечь/Добавить аудио)")
